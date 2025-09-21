5365633224053100e932edc58b39a127e5261770
agga <hanzawnyineonline@gmail.com>
2025-09-21 21:39:07 +0630

    [ADD] add booking

--- addons/ica_hms/__manifest__.py
+++ addons/ica_hms/__manifest__.py
@@ -1,10 +1,14 @@
 {
     "name":"Hotel Management System",
     "author":"Agaa, IdeaCode Academy",
+    "depends":["mail"],
     "data":[
+        "data/ir_sequence.xml",
         "security/ir.model.access.csv",
+        "views/hms_booking.xml",
         "views/hms_room.xml",
         "views/hms_hotel.xml",
+
         "views/menus.xml",
     ],
     "license":"LGPL-3"
---
+++ addons/ica_hms/models/hms_booking.py
@@ -0,0 +1,71 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+class HmsBooking(models.Model):
+    _name = 'hms.booking'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = 'HmsBooking'
+    _rec_name = "partner_id"
+
+    partner_id = fields.Many2one('res.partner', required=True, tracking=True)
+    room_id = fields.Many2one('hms.room', required=True, tracking=True)
+    hotel_id = fields.Many2one('hms.hotel', related="room_id.hotel_id")
+    currency_id = fields.Many2one('res.currency', related="room_id.currency_id")
+    amount = fields.Monetary(tracking=True)
+    reference = fields.Char(default=lambda self: _("New"), readonly=True)
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('confirmed', 'Confirmed'),
+        ('paid', 'Paid'),
+        ('check_in', 'Check In'),
+        ('check_out', 'Check Out'),
+        ('cancelled', 'Cancelled'),
+    ], default='draft', tracking=True)
+
+    @api.onchange('room_id')
+    def _onchange_room(self):
+        if self.room_id:
+            self.amount = self.room_id.amount
+
+    @api.model
+    def create(self, values):
+        # Add code here
+        for value in values:
+            if not value.get('reference') or value.get('reference') == _("New"):
+                value['reference'] = self.env['ir.sequence'].next_by_code('hms.booking')
+        return super(HmsBooking, self).create(values)
+
+    @staticmethod
+    def _allow_states():
+        return [
+            ('draft', 'confirmed'),
+            ('confirmed', 'paid'),
+            ('paid', 'check_in'),
+            ('check_in', 'check_out'),
+            ('confirmed', 'cancelled'),
+            ('draft', 'cancelled'),
+        ]
+
+    def _change_state(self,new_state):
+        if (self.state,new_state) not in self._allow_states():
+            raise UserError(_(f'{self.state} to {new_state} is not allowed.'))
+        self.state=new_state
+
+    def action_draft(self):
+        self._change_state('draft')
+
+    def action_confirm(self):
+        self._change_state('confirmed')
+
+    def action_paid(self):
+        self._change_state('paid')
+
+    def action_check_in(self):
+        self._change_state('check_in')
+
+    def action_check_out(self):
+        self._change_state('check_out')
+
+    def action_cancel(self):
+        self._change_state('cancelled')
