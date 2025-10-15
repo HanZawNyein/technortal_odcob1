cb9e5a4ffb841f1d1a2f191a0679d73ef94085b7
agga <hanzawnyineonline@gmail.com>
2025-10-15 21:25:50 +0630

    [ADD] company & settings, context, config_parameter

--- addons/ica_hms/models/res_config_settings.py
+++ addons/ica_hms/models/res_config_settings.py
@@ -6,3 +6,5 @@ class ResConfigSettings(models.TransientModel):
 
     hms_default_payment_method= fields.Many2one('hms.payment.method', string='Payment Method',
                                                 related='company_id.hms_default_payment_method',readonly=False)
+
+    is_available = fields.Boolean(string='Available', default=False,config_parameter='hsm.is_available')
