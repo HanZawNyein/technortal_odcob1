ea2a1b6973d6d12ea90a20a0d7afccb4b62aa924
HanZawNyein <hanzawnyineonline@gmail.com>
2025-11-26 20:46:10 +0630

    [ADD] userService and owl services

--- addons/ica_hms/static/src/hms_room/hms_room_client_action.js
+++ addons/ica_hms/static/src/hms_room/hms_room_client_action.js
@@ -1,12 +1,55 @@
-import { registry } from "@web/core/registry";
-import { Layout } from "@web/search/layout";
-import { Component } from  "@odoo/owl";
+import {registry} from "@web/core/registry";
+import {Layout} from "@web/search/layout";
+import {Component, useState, onWillStart} from "@odoo/owl";
+import {useService} from "@web/core/utils/hooks";
+
 
 class MyClientAction extends Component {
     static template = "ica_hms.hms_room";
     static components = {Layout};
 
-    printWeb(){
+    setup() {
+        this.state = useState({
+            value: 100,
+            partners: []
+        });
+        this.orm = this.env.services.orm;
+        this.effect = useService('effect');
+        this.titleService = useService("title");
+        this.titleService.setParts({ odoo: "Odoo 15", fruit: "Apple" });
+
+
+        console.log(this.env.services.orm)
+        onWillStart(async () => {
+            await this.getPartners();
+        })
+    }
+
+    showEffect() {
+        this.effect.add({
+            type: "rainbow_man", // can be omitted, default type is already "rainbow_man"
+            message: "Boom! Team record for the past 30 days.",
+        });
+    }
+
+    async getPartners() {
+        this.state.partners = await this.orm.searchRead('res.partner', [], ['id', 'display_name', 'create_uid', 'create_date']);
+        console.log(this.state.partners)
+    }
+
+    async deletePartners(partnerId) {
+        await this.orm.unlink('res.partner', [partnerId]);
+        console.log("partner is delete")
+        console.log(partnerId)
+        this.state.partners = this.state.partners.filter(existing_partner => existing_partner.id !== partnerId);
+    }
+
+    value_add() {
+        this.state.value++;
+        console.log(this.state.value);
+    }
+
+    printWeb() {
         console.log("hello")
     }
 }
