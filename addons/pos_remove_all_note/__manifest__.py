76d88c53202e77530e3fd65e84d5de2917afa7b9
HanZawNyein <hanzawnyineonline@gmail.com>
2025-12-03 20:56:48 +0630

    [ADD] actions
    server action
    url action

---
+++ addons/pos_remove_all_note/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name":"POS Remove All Note",
+    "summary":"Remove all notes",
+    "author":"Agga, IdeaCode Academy",
+    "depends":["point_of_sale"],
+    "data": [],
+    "assets":{
+        'point_of_sale._assets_pos': [
+            "pos_remove_all_note/static/src/**/*",
+        ],
+    },
+    "license":"LGPL-3"
+}
---
+++ addons/pos_remove_all_note/static/src/app/screens/product_screen/control_buttons/control_buttons.js
@@ -0,0 +1,9 @@
+import {ControlButtons} from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
+import {patch} from "@web/core/utils/patch";
+
+
+patch(ControlButtons.prototype, {
+    getRemoveAllNotes() {
+        this.currentOrder.lines.map(line=>line.note=undefined);
+    }
+})
